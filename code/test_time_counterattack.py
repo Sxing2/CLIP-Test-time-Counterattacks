cede94c35c13a8c8e1e34c1b78a94bd4f14d698c
Sxing2 <130158582+Sxing2@users.noreply.github.com>
2025-03-09 23:44:22 +0100

    Update test_time_counterattack.py

--- code/test_time_counterattack.py
+++ code/test_time_counterattack.py
@@ -11,7 +11,7 @@ from copy import deepcopy as dcopy
 import torch
 from torch.cuda.amp import GradScaler, autocast
 
-import clip
+import replace.clip
 from models.prompters import TokenPrompter, NullPrompter
 from utils import *
 from attacks import *
